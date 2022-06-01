55c30a7eccbd221e3f23bfa9cf84d0bd2e58815d
Ansarissab <zahidensari116@gmail.com>
2022-06-01 13:15:56 +0500

    Added qr code functionality

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,16 @@
 class Site < ApplicationRecord
+  before_save :generate_qr
+
+  private
+
+  def generate_qr
+    qrcode = RQRCode::QRCode.new(self.url)
+    self.svg = qrcode.as_svg(
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 11,
+      standalone: true,
+      use_path: true
+    )
+  end
 end
